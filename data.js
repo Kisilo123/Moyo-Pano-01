aaeda066f5f35cce8fb5c6cdba69d6bdf2132f9f
Kisilo123 <119424488+Kisilo123@users.noreply.github.com>
2022-11-29 16:44:57 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,52 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-02",
+      "name": "02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.5996746624895763,
+        "pitch": 0.27271926755916986,
+        "fov": 1.4333212056084728
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5996746624895763,
+          "pitch": 0.27271926755916986,
+          "rotation": 0,
+          "target": "0-02"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.014583110421430234,
+          "pitch": -0.08425973768351369,
+          "title": "&nbsp;Rooftop",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
